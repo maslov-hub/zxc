8591a61e5a5c331cf14f7652b156d89a225fe530
maslov-hub <jsjhfk@mail.ru>
2025-12-12 13:22:17 +0300

    2 commit

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program {
 
-static void Main()
+static void Main()//2 commit
         {
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var result = numbers.Skip(3).ToArray();
