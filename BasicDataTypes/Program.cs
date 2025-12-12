9d3b9569c0b2afa49368d2e019370df3f35ca3ed
maslov-hub <jsjhfk@mail.ru>
2025-12-12 13:19:55 +0300

    first commit

---
+++ BasicDataTypes/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program {
+
+static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = numbers.Skip(3).ToArray();
+            Console.WriteLine($"Task 6: {string.Join(", ", result)}");
+        }
+        }
